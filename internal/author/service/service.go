639038940a8c9e06c96d0e2cfa47d9f14722ce60
papvan <papvann@gmail.com>
2022-08-23 00:41:21 +0300

    temporary fix

--- internal/author/service/service.go
+++ internal/author/service/service.go
@@ -6,7 +6,6 @@ import (
 	"papvan/cvmaker/internal/author/model"
 	"papvan/cvmaker/internal/author/storage"
 	model2 "papvan/cvmaker/internal/author/storage/model"
-	"papvan/cvmaker/pkg/api/filter"
 	"papvan/cvmaker/pkg/api/sorting"
 	"papvan/cvmaker/pkg/logging"
 )
@@ -20,7 +19,8 @@ func NewService(repository storage.Repository, logger *logging.Logger) *Service 
 	return &Service{repository: repository, logger: logger}
 }
 
-func (s *Service) GetAll(ctx context.Context, filterOptions filter.Options, sortOptions sorting.Options) ([]model.Author, error) {
+//func (s *Service) GetAll(ctx context.Context, filterOptions filter.Options, sortOptions sorting.Options) ([]model.Author, error) {
+func (s *Service) GetAll(ctx context.Context, sortOptions sorting.Options) ([]model.Author, error) {
 	options := model2.NewSortOptions(sortOptions.Field, sortOptions.Order)
 	all, err := s.repository.FindAll(ctx, options)
 	if err != nil {
